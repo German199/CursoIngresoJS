41cc69364de82ff233f435f361fc596af016c21d
German199 <52494513+German199@users.noreply.github.com>
2019-07-03 11:46:23 -0300

    4 primeros ejercicios del curso de ingreso
    esto da verguenza ajena

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,13 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+    //tomo edad
+    var nombre;
+    //nombre = "Maria";
+    nombre= prompt("ingresa tu nombre atr", "no tu apodo");
+    alert("su nombre es : "+nombre);
+
+
 	
 }
 
--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -2,8 +2,18 @@
 	Debemos lograr tomar un dato por 'PROMPT' 
 	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
 */
-function mostrar()
+//function mostrar()
 {
+
+
+//var algo=document.getelemntbyID(elID).value....ID entrada
+//document.getelemntByID(elID).value = algo....ID salida
+
+var dato;
+var valor;
+dato=prompt
+valor=document.getElementById('elnombre.value');
+alert(dato);
 
 }
 
