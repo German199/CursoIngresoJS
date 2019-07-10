8235609617ebb037555c7a27e23c408671206e99
German199 <52494513+German199@users.noreply.github.com>
2019-07-10 11:43:52 -0300

    Add files via upload

---
+++ jsInstruccionIF (2).js
@@ -0,0 +1,12 @@
+function mostrar()
+{
+//tomo la edad  
+    var edad;
+    edad=document.getElementById("edad").value;
+    if(edad<18){
+        alert("pendejo");
+    }else if(edad>18){
+        alert("viejo");
+    }
+
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionIF (5).js
@@ -0,0 +1,15 @@
+function mostrar()
+{
+//tomo la edad  
+    var edad;
+    edad=document.getElementById("edad").value;
+    if(edad>18){
+        alert("no es adolescente");
+        }
+        else if(edad<13){
+            alert("no es adolescente");
+
+        }
+    
+
+}//FIN DE LA FUNCIÓN
