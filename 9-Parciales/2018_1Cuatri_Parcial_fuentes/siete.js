19f22de3f12bc2d9ed60a5b885da615cb99efde5
German199 <52494513+German199@users.noreply.github.com>
2019-08-01 12:11:58 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,36 @@
 function mostrar()
 {
+    var sexo;
+    var nota;
+    var contador;
+    var acumulador;
+    var notamasbaja;
+    var promedio;
+    contador=0;
+    acumulador=0;
+
+    while(contador<5){
+        sexo=prompt("ingrese m o f segun corresponda aqui.");
+       
+            while(sexo!='f'&&sexo!='m'){
+            
+            sexo=prompt("ingreso dato valido");
+            }
+       
+        nota=parseInt(prompt("ingrese notas aqui."));
+
+            acumulador+=nota;
+        
+            while(isNaN(nota) || nota<0 || nota>10){//isNaN noes numero
+            
+            nota=prompt("ingrese una nota valida");
+       }
+        contador++;
+    }
+        promedio=acumulador/contador;
+        alert(promedio);
+        notamasbaja=nota>acumulador;
+        alert(notamasbaja);
+
 
 }
